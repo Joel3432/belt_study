8fe1f36d8fc7dcaec9e3335c7b675293d4d4395b
Joel Martin <81052368+Joel3432@users.noreply.github.com>
2022-09-08 12:27:15 -0400

    Added coded for all necessary post hocs immediately after their original ARTool analysis

--- 20220901_QSDC7_COPEMG_Code.R
+++ 20220901_QSDC7_COPEMG_Code.R
@@ -70,83 +70,197 @@ df = data.frame(subject,condition,task,LRF,RRF,LBF,RBF,LAB,RAB,LMF,RMF,rangeAP,r
 #summary to check that art was done properly ( should be 0's)
 #run as ANOVA
 
+#Analysis 
 artLAB = art(LAB ~ condition * task + (1|subject), data=df)
 summary(artLAB)
 anova(artLAB)
-
-artRAB = art(RAB ~ condition * task + (1|subject), data=df)
-summary(artRAB)
-anova(artRAB)
-
-artLMF = art(LMF ~ condition * task + (1|subject), data=df)
-summary(artLMF)
-anova(artLMF)
-
-artRMF = art(RMF ~ condition * task + (1|subject), data=df)
-summary(artRMF)
-anova(artRMF)
-
-artLRF = art(LRF ~ condition * task + (1|subject), data=df)
-summary(artLRF)
-anova(artLRF)
-
-artRRF = art(RRF ~ condition * task + (1|subject), data=df)
-anova(artRRF)
-
-artLBF = art(LBF ~ condition * task + (1|subject), data=df)
-summary(artLBF)
-anova(artLBF)
-
-artRBF = art(RBF ~ condition * task + (1|subject), data=df)
-summary(artRBF)
-anova(artRBF)
-
-artrangeAP = art(rangeAP ~ condition * task + (1|subject), data=df)
-summary(artrangeAP)
-anova(artrangeAP)
-
-artrangeML = art(rangeML ~ condition * task + (1|subject), data=df)
-summary(artrangeML)
-anova(artrangeML)
-
-artmeanvel = art(meanvel ~ condition * task + (1|subject), data=df)
-summary(artmeanvel)
-anova(artmeanvel)
-
-artmeanvelAP = art(meanvelAP ~ condition * task + (1|subject), data=df)
-summary(artmeanvelAP)
-anova(artmeanvelAP)
-
-artmeanvelML = art(meanvelML ~ condition * task + (1|subject), data=df)
-summary(artmeanvelML)
-anova(artmeanvelML)
-
-artellipse = art(ellipsearea ~ condition * task + (1|subject), data=df)
-summary(artellipse)
-anova(artellipse)
-
-#Post-hoc testing for significant effects
-
-#Left abdominal
+#Post Hoc Condition
 art.con(artLAB, ~condition, adjust="bonferroni") %>%  # run ART-C for X1
   summary() %>%  # add significance stars to the output
   mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
                        cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
                        symbols = c("***", "**", "*", ".", " ")))
-
+#Post Hoc Task 
 art.con(artLAB, ~task, adjust="bonferroni") %>%  # run ART-C for X1
   summary() %>%  # add significance stars to the output
   mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
                        cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
                        symbols = c("***", "**", "*", ".", " ")))
-
+#Post Hoc Interaction
 art.con(artLAB, "condition:task", adjust="bonferroni") %>%  # run ART-C for X1 × X2
   summary() %>%  # add significance stars to the output
   mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
                        cutpoints = c(0, .001, .01, .05, .10, 1),
                        symbols = c("***", "**", "*", ".", " ")))
 
+#Analysis
+artRAB = art(RAB ~ condition * task + (1|subject), data=df)
+summary(artRAB)
+anova(artRAB)
+#Post Hoc Condition
+art.con(artRAB, ~condition, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Post Hoc Task 
+art.con(artRAB, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Post Hoc Interaction
+art.con(artRAB, "condition:task", adjust="bonferroni") %>%  # run ART-C for X1 × X2
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, .001, .01, .05, .10, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artLMF = art(LMF ~ condition * task + (1|subject), data=df)
+summary(artLMF)
+anova(artLMF)
+# Post Hoc Task 
+art.con(artLMF, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Post Hoc Interaction
+art.con(artLMF, "condition:task", adjust="bonferroni") %>%  # run ART-C for X1 × X2
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, .001, .01, .05, .10, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis 
+artRMF = art(RMF ~ condition * task + (1|subject), data=df)
+summary(artRMF)
+anova(artRMF)
+#Post Hoc Condition
+art.con(artRMF, ~condition, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Post Hoc Task 
+art.con(artRMF, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Post Hoc Interaction
+art.con(artRMF, "condition:task", adjust="bonferroni") %>%  # run ART-C for X1 × X2
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, .001, .01, .05, .10, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artLRF = art(LRF ~ condition * task + (1|subject), data=df)
+summary(artLRF)
+anova(artLRF)
+#Post Hoc Condition
+art.con(artLRF, ~condition, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artRRF = art(RRF ~ condition * task + (1|subject), data=df)
+summary(artRRF)
+anova(artRRF)
+#Post Hoc Task 
+art.con(artRRF, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artLBF = art(LBF ~ condition * task + (1|subject), data=df)
+summary(artLBF)
+anova(artLBF)
+#Post Hoc Condition
+art.con(artLBF, ~condition, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Post Hoc Task 
+art.con(artLBF, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
 
+#Analysis
+artRBF = art(RBF ~ condition * task + (1|subject), data=df)
+summary(artRBF)
+anova(artRBF)
+#Post Hoc Condition
+art.con(artRBF, ~condition, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artrangeAP = art(rangeAP ~ condition * task + (1|subject), data=df)
+summary(artrangeAP)
+anova(artrangeAP)
+#Post Hoc Task 
+art.con(artrangeAP, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+
+#Analysis
+artrangeML = art(rangeML ~ condition * task + (1|subject), data=df)
+summary(artrangeML)
+anova(artrangeML)
+#Post Hoc Task
+art.con(artrangeML, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artmeanvel = art(meanvel ~ condition * task + (1|subject), data=df)
+summary(artmeanvel)
+anova(artmeanvel)
+#Post Hoc Task 
+art.con(artmeanvel, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artmeanvelAP = art(meanvelAP ~ condition * task + (1|subject), data=df)
+summary(artmeanvelAP)
+anova(artmeanvelAP)
+#Post Hoc Task 
+art.con(artmeanvelAP, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artmeanvelML = art(meanvelML ~ condition * task + (1|subject), data=df)
+summary(artmeanvelML)
+anova(artmeanvelML)
+#Post Hoc Task 
+art.con(artmeanvelML, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
+#Analysis
+artellipse = art(ellipsearea ~ condition * task + (1|subject), data=df)
+summary(artellipse)
+anova(artellipse)
+#Post Hoc Task 
+art.con(artellipse, ~task, adjust="bonferroni") %>%  # run ART-C for X1
+  summary() %>%  # add significance stars to the output
+  mutate(sig. = symnum(p.value, corr=FALSE, na=FALSE,
+                       cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                       symbols = c("***", "**", "*", ".", " ")))
 
 
 #Grouped Bar Chart visuals if applicable (only if "WinsByTask" is uploaded)
